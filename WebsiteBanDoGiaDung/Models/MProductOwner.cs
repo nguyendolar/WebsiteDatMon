b2b8a3eac50ee3e384ab19a45c891bd36a908b68
nguyendolar <nguyencaonguyencmu@gmail.com>
2022-03-02 14:14:42 +0700

    first commit

---
+++ WebsiteBanDoGiaDung/Models/MProductOwner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteBanDoGiaDung.Models
+{
+    [Table("ProductOwner")]
+    public class MProductOwner
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created_at {get;set;}
+        public int Created_by {get;set;}
+        public DateTime Updated_at {get;set;}
+        public int Updated_by { get; set; }
+        public int Status { get; set; }
+    }
+}
